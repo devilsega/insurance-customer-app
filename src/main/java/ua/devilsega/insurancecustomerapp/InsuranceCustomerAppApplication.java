75a93d83c623a81d867e077fbca3f6ce8d1eaf33
Sergey Sorokin <devilsega@yandex.ru>
2018-11-17 00:12:17 +0200

    Barebone maven project structure added.

---
+++ src/main/java/ua/devilsega/insurancecustomerapp/InsuranceCustomerAppApplication.java
@@ -0,0 +1,12 @@
+package ua.devilsega.insurancecustomerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCustomerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceCustomerAppApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/devilsega/insurancecustomerapp/InsuranceCustomerAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.devilsega.insurancecustomerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsuranceCustomerAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
